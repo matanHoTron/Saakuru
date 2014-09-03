708fd3a37654e24ca897c8b9d6daf6329c6ca6e2
unknown <chais@WS-Lib004.IASA.ORG>
2014-09-03 15:56:20 +0300

    Added Project Files

---
+++ Saakuru/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Saakuru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************");
+            Console.WriteLine("* Prints a random integer from 1 to k *");
+            Console.WriteLine("* With probability proportional to k  *");
+            Console.WriteLine("* In a linearly                       *");
+            Console.WriteLine("***************************************");
+            Console.WriteLine();
+            Console.WriteLine("Enter a k: ");
+            int k = int.Parse(Console.ReadLine());
+
+            UnfairRandom unfair = new UnfairRandom(k);
+            Console.WriteLine("Unfair Number: " + unfair.UnfairRandom());
+            Console.WriteLine("Fair Number:   " + unfair.FairRandom());
+        }
+    }
+}
---
+++ Saakuru/UnfairRandom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Saakuru
+{
+    class UnfairRandom
+    {
+        private int k;
+        private Random r;
+        public UnfairRandom(int _k)
+        {
+            this.k = _k;
+            this.r = new Random();
+        }
+
+        public int FairRandom()
+        {
+            return r.Next(1, this.k + 1);
+        }
+
+        public int UnfairRandom()
+        {
+            int k_tri = (k + 1) * k / 2;
+            int n = r.Next(1, k_tri + 1);
+            return (int)(Math.Sqrt(8 * n + 1) - 1 / 2);
+        }
+    }
+}
